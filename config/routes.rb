21292df9302aaf66cedd1d5e254513353caf6922
simp <donlancaster228@yandex.ru>
2021-02-25 12:33:00 +0300

    xz

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 Rails.application.routes.draw do
   root 'welcome_page#begin'
   get 'welcome_page/begin'
+  get 'signup' => 'users#new'
+  resources :users
   get 'login' => 'login_page'
   get 'registration' => 'registration'
 end
